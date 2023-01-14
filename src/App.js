fdc6a87c153c8062b10e551bd541ce7dc81163d0
bhar75 <bharathi_n@msn.com>
2023-01-14 14:11:22 +0530

    add remove functionality to button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -31,9 +36,23 @@ const App = () => {
       </main>
     );
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left !</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 };
